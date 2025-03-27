fdbe2fca8d52a3be4905d0fa8912fb5c0dac11e6
Kanimozhi-RAVI <mozhi9605@gmail.com>
2025-03-27 17:00:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+/*let a, b, c;
+a=10;
+b=5;
+c=a+b;*/
+console.log(c);
+console.log('kanimozhi');
+console.log([58,20,10]);
+console.table({fname:'kani',age:'21'});
+console.error("custom arror");
+
+
+
+{
+    let a=10;
+    console.log(a);
+}
+var a=10;
+var b=28;
+var c=a+b;
+console.log(c);
+const e=10;
+const f=65;
+const d=e+f;
+console.log(d);
+ let x=50;
+ x>>=2;
+ console.log(x);
+ let y = 100;
+ y |= 5;
+ console.log(y);
+
+
+ let fname=60;
+ fname&=50;
+ console.log(fname);
+ let lname =40;
+ lname|=10;
+ console.log(lname);
+ let xy=58;
+ xy^=22;
+ console.log(xy);
+  
+
+
+ 
